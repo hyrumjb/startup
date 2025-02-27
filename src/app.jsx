5de4ebed51f966d203d3decbd79aab4cc52d5dbe
Hyrum Bradshaw <bradshaw.hyrum@gmail.com>
2025-02-27 15:55:05 -0700

    updated app.jsx with new javascript

--- src/app.jsx
+++ src/app.jsx
@@ -6,50 +6,72 @@ import { Login } from './login/login';
 import { Finances } from './finances/finances';
 import { Shared } from './shared/shared';
 import { About } from './about/about';
+import { AuthState } from './login/authState';
 
- function App() {
-  return (
-    <BrowserRouter>
-        <div className="custom-bg">
-            <header className="container">
-                <div className="d-flex flex-wrap align-items-center justify-content-between py-3 mb-4 border-bottom">
-                    <div className="col-md-6 d-flex align-items-center">
-                        <h1 className="mb-0 text-light">myDinero</h1>
+function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+
+    return (
+        <BrowserRouter>
+            <div className="custom-bg">
+                <header className="container">
+                    <div className="d-flex flex-wrap align-items-center justify-content-between py-3 mb-4 border-bottom">
+                        <div className="col-md-6 d-flex align-items-center">
+                            <h1 className="mb-0 text-light">myDinero</h1>
+                        </div>
+                        <div className="col-md-6 text-center d-none d-md-block">
+                            <h6 className="mb-0 text-light">~ the simple money interface ~</h6>
+                        </div>
+                        <nav className="navbar sticky-top navbar-dark">
+                            <menu className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                                <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="">Home</NavLink></li>
+                                {authState == AuthState.Authenticated && (
+                                    <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="finances">My Investments</NavLink></li>
+                                )}
+                                {authState == AuthState.Authenticated && (
+                                    <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="shared">Shared Investments</NavLink></li>
+                                )}
+                                <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="about">About</NavLink></li>
+                            </menu>
+                        </nav>
                     </div>
-                    <div className="col-md-6 text-center d-none d-md-block">
-                        <h6 className="mb-0 text-light">~ the simple money interface ~</h6>
+                    <hr></hr>
+                </header>
+
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+                            <Login
+                                userName={userName}
+                                authState={authState}
+                                onAuthChange={(userName, authState) => {
+                                    setAuthState(authState);
+                                    setUserName(userName);
+                                }}
+                            />
+                            } 
+                            exact 
+                    />
+                    <Route path='/finances' element={<Finances userName={UserName} />} />
+                    <Route path='/shared' element={<Shared />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+
+                <footer className="bg-dark text-white-50">
+                    <div className="container-fluid">
+                        <hr />
+                        <span className="text-reset">Hyrum Bradshaw</span>
+                        <br />
+                        <a className="text-reset" href="https://github.com/hyrumjb/startup">GitHub</a>
                     </div>
-                    <nav className="navbar sticky-top navbar-dark">
-                        <menu className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                            <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="">Home</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="finances">My Investments</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="shared">Shared Investments</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link active px-2 link-secondary" to="about">About</NavLink></li>
-                        </menu>
-                    </nav>
-                </div>
-                <hr></hr>
-            </header>
-
-            <Routes>
-                <Route path='/' element={<Login />} exact />
-                <Route path='/finances' element={<Finances />} />
-                <Route path='/shared' element={<Shared />} />
-                <Route path='/about' element={<About />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-
-            <footer className="bg-dark text-white-50">
-                <div className="container-fluid">
-                    <hr />
-                    <span className="text-reset">Hyrum Bradshaw</span>
-                    <br />
-                    <a className="text-reset" href="https://github.com/hyrumjb/startup">GitHub</a>
-                </div>
-            </footer>
-        </div>
-    </BrowserRouter>
-  );
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 function NotFound() {
