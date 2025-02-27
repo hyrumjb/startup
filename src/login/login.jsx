7b5b18fc117a05ce1650a1e4ff3f44aafe34d487
Hyrum Bradshaw <bradshaw.hyrum@gmail.com>
2025-02-27 16:09:41 -0700

    added more functionality to the login.jsx page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main class="container-fluid bg-secondary text-center">
-        <h1>Welcome to myDinero</h1>
-        <form method="get" action="play.html">
+        {authState !== AuthState.Unknown && <h1>Welcome to myDinero</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+        {/* <form method="get" action="play.html">
             <div class="input-group mb-3">
                 <span class="input-group-text">Username</span>
                 <input class="form-control" type="text" placeholder="example123" />
@@ -16,7 +31,7 @@ export function Login() {
             </div>
             <button type="submit" class="btn btn-primary">Login</button>
             <button type="submit" class="btn btn-secondary">Create Account</button>
-        </form>
+        </form> */}
     </main>
   );
 }
