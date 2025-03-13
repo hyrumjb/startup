24d52c7128591fa8e163b831ab19551d88770e74
Hyrum Bradshaw <bradshaw.hyrum@gmail.com>
2025-03-13 16:17:24 -0600

    finished updating index.js

--- service/index.js
+++ service/index.js
@@ -15,3 +15,102 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
+
+        setAuthCookies(res, user.token);
+        res.send({ email: user.email });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4();
+            setAuthCookies(res, user.token);
+            res.send({ email: user.email });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
+
+apiRouter.get('/finances', verifyAuth, (_req, res) => {
+    res.send(finances)
+});
+
+apiRouter.post('/finances', verifyAuth, (req, res) => {
+    finances = updateFinances(req.body);
+    res.send(finances);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+function updateFinances(newFinance) {
+    finances.push(newFinance);
+    return finances;
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+
+    return user;
+}
+
+async function findUser(field, value) {
+    if (!value) return null;
+
+    return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
